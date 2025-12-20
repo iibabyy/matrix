8497553810c815e398fc7d76d88e111a4597d262
ibaby <utileidrissa@gmail.com>
2025-12-20 21:12:26 +0100

    added subject test cases for linear_combination

--- src/vector/linear_combination.rs
+++ src/vector/linear_combination.rs
@@ -29,13 +29,20 @@ mod tests {
     }
 
     #[test]
-    fn test_subject_case() {
+    fn test_subject_case_1() {
         let e1 = Vector::from([1., 0., 0.]);
         let e2 = Vector::from([0., 1., 0.]);
         let e3 = Vector::from([0., 0., 1.]);
+
+        assert_eq!(Vector::from(vec![10., -2., 0.5]), linear_combination::<f32>(&[e1, e2, e3], &[10., -2., 0.5]));
+    }
+
+    #[test]
+    fn test_subject_case_2() {
         let v1 = Vector::from([1., 2., 3.]);
         let v2 = Vector::from([0., 10., -100.]);
-        assert_eq!(Vector::from(vec![10., -2., 0.5]), linear_combination::<f32>(&[e1, e2, e3], &[10., -2., 0.5]));
+
+        assert_eq!(Vector::from(vec![10., 0., 230.]), linear_combination::<f32>(&[v1, v2], &[10., -2.]));
     }
 
     #[test]
