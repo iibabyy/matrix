af3189be49c078bb2881953321e21131a3a50bfb
ibaby <utileidrissa@gmail.com>
2025-12-26 16:07:58 +0100

    tweak macros

--- src/macros.rs
+++ src/macros.rs
@@ -2,21 +2,21 @@
 #![allow(unused_imports)]
 
 macro_rules! vector {
-    ($elem:expr; $n:expr) => (
+    ($elem:expr; $n:expr) => {
         $crate::vector::Vector::from_elem($elem, $n)
-    );
-    ($($x:expr),+ $(,)?) => (
+    };
+    ($($x:expr),+ $(,)?) => {
         $crate::vector::Vector::from([$($x),+])
-    );
+    };
 }
 
 macro_rules! matrix {
-    ($elem:expr; $n:expr) => (
+    ($elem:expr; $n:expr) => {
         $crate::matrix::Matrix::from_elem($elem, $n)
-    );
-    ($($x:expr),+ $(,)?) => (
+    };
+    ($($x:expr),+ $(,)?) => {
         $crate::matrix::Matrix::from([$($x),+])
-    );
+    };
 }
 
 pub(crate) use matrix;
