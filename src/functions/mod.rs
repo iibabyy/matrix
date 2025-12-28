295968ea8fd3e7548618c7b49f2e7dc17f402e53
ibaby <utileidrissa@gmail.com>
2025-12-28 23:02:25 +0100

    moved function files (linear_combination.rs, linear_interpolation.rs) into src/function/)

---
+++ src/functions/mod.rs
@@ -0,0 +1,5 @@
+pub mod linear_combination;
+pub use linear_combination::*;
+
+pub mod linear_interpolation;
+pub use linear_interpolation::*;
--- src/lib.rs
+++ src/lib.rs
@@ -25,8 +25,5 @@ mod macros;
 mod matrix;
 mod vector;
 
-mod linear_interpolation;
-pub use linear_interpolation::{lerp, lerp_generic};
-
-mod linear_combination;
-pub use linear_combination::linear_combination;
+mod functions;
+pub use functions::*;
